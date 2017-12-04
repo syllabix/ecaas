d8783fdab6b82652dc66df2779fc8268bd3ee4af
Tom Stoepker <tom.stoepker@cqlcorp.com>
2017-12-04 11:05:49 -0500

    setting up demo

--- estimator.go
+++ estimator.go
@@ -1,7 +1,7 @@
 package ecaas
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/shopspring/decimal"
 )
@@ -16,44 +16,19 @@ import (
 //		complexity factor to get a estimate high end subtotal
 //  5. Apply tax to the high end of the estimate, end return the range formatted in USD
 func CalculateTotalCost(details *JobDetails, moveType MoveType) *EstimateRange {
-
-	totalHours := decimal.NewFromFloat(details.Hours)
-	hourlyRate, _ := decimal.NewFromString(details.HourlyRate)
-
-	taxRate, _ := decimal.NewFromString(moveType.GetTaxRate())
-	jobMultiplier, _ := decimal.NewFromString(moveType.GetMultiplier())
-
-	complexity, _ := jobComplexityForDate(details.PreferredMoveDate)
-	complexityFactor := decimal.NewFromFloat(complexity)
-
-	lowTotal := addPercentageToTotal(totalHours.Mul(hourlyRate), jobMultiplier)
-	lowTotalWithTax := addTax(lowTotal, taxRate)
-	highTotal := addPercentageToTotal(lowTotal, complexityFactor)
-	highTotalWithTax := addTax(highTotal, taxRate)
-
-	return &EstimateRange{
-		Low:  fmt.Sprintf("$%s", lowTotalWithTax.StringFixedBank(2)),
-		High: fmt.Sprintf("$%s", highTotalWithTax.StringFixedBank(2)),
-	}
-}
-
-func addPercentageToTotal(total decimal.Decimal, percentage decimal.Decimal) decimal.Decimal {
-	return total.Mul(percentage.Add(decimal.NewFromFloat(1)))
+	//TODO - Implement
+	return nil
 }
 
 func addTax(total decimal.Decimal, taxRate decimal.Decimal) decimal.Decimal {
 	return addPercentageToTotal(total, taxRate)
 }
 
+func addPercentageToTotal(total decimal.Decimal, percentage decimal.Decimal) decimal.Decimal {
+	return total.Mul(percentage.Add(decimal.NewFromFloat(1)))
+}
+
 func jobComplexityForDate(dateString string) (float64, error) {
-	// date, err := time.Parse(time.UnixDate, dateString)
-	// if err != nil {
-	// 	return 0, ErrorInvalidDateFormat
-	// }
-	// complexity := 0.15
-	// if date.Weekday() == time.Friday ||
-	// 	date.Weekday() == time.Saturday {
-	// 	complexity = 0.3
-	// }
-	// return complexity, nil
+	//TODO: Implement function
+	return 0, errors.New("Not Implemented")
 }
